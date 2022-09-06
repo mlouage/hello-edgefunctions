c7fb3cfbf246bc8447d2c45a9718247123131e85
Maarten Louage <me@maartenlouage.nl>
2022-09-06 17:21:04 +0200

    Hello from country

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,7 +1,7 @@
 import type { Context } from "https://edge.netlify.com";
 
 export default async (_req: Request, { geo }: Context): Promise<Response> => {
-  const res = new Response(`Hello from ${geo.city}!`);
+  const res = new Response(`Hello from ${geo.country?.name}!`);
   const text = await res.text();
 
   return new Response(text.toUpperCase(), res);
